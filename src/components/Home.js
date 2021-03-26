5da1cd9366004b15a698b5b0a5d99191a87c2782
diana2341 <dianaponce2341@gmail.com>
2021-03-26 15:00:50 -0400

    nav and home

---
+++ src/components/Home.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import {Carousel} from 'react-bootstrap'
+import tac1 from '../images/tac4.jpeg';
+import tac2 from '../images/tac5.jpg';
+import tac3 from '../images/tac3.jpeg';
+import bur1 from '../images/bur1.jpeg';
+
+
+
+class Home extends Component{
+
+    
+
+    
+    render(){
+        return(
+    <div>
+            
+        <Carousel fade interval={3000}>
+            <Carousel.Item>
+                <img
+                //  height={664}
+                className="d-block w-100"
+                src={tac1}
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                //  height={664}
+                className="d-block w-100"
+                src={tac2}
+                alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                 
+                //  height={664}
+                className="d-block w-100"
+                src={bur1}
+                alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    </div>
+        )
+    }                
+
+}
+export default Home
---
+++ src/components/NavbarComponent.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import logo from '../images/chil.png'
+import {Navbar} from 'react-bootstrap';
+import {Nav} from 'react-bootstrap';
+import {NavDropdown} from 'react-bootstrap';
+
+
+export default class NavbarComponent extends React.Component{
+    render(){
+        return(
+      <Navbar bg="transparent" expand="lg" sticky="top">
+        <Navbar.Brand href="#home">
+      <img
+        src={logo}
+        width="100"
+        height="auto"
+        className="d-inline-block align-top"
+        alt="logo"
+      />
+    </Navbar.Brand>
+    <Navbar.Brand id='logo-title'href="#home">Taco Haven</Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <NavDropdown title="Menu" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Breakfast</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Lunch</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Dinner</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+        )
+    }
+}
