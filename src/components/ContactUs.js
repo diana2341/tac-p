56eb7f191603c90ce2a1ef1ce7ca3415ea95e183
diana2341 <dianaponce2341@gmail.com>
2021-03-31 11:06:37 -0400

    menu iphone

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -9,31 +9,31 @@ export default class ContactUs extends React.Component{
                 {/* <Jumbotron> */}
                     {/* <Container><br/><br/> */}
                     <div className='jumbo'>
-                    <Figure>
-                <Figure.Image
-                width={210}
-                height={310} 
-                alt="171x180"
-                src={logo}
-                className='skull'
-                />
-                </Figure>
-                    <h1 className='title-food'>Contact Us</h1>
-                    <p>Interested in joining our team? </p>
-                    <p>Click <a href=''>here</a> for more information about open positions</p>
+                        <Figure>
+                            <Figure.Image
+                            width={210}
+                            height={310} 
+                            alt="171x180"
+                            src={logo}
+                            className='skull'
+                            />
+                        </Figure>
+                        <h1 className='title-food'>Contact Us</h1>
+                        <p>Interested in joining our team? </p>
+                        <p>Click <a href=''>here</a> for more information about open positions</p>
 
 
-                    <p>This is a great place to host your holiday parties, business lunches, dinners, baby showers and much more! If you'd like to visit Us to discuss options For reservations, contact our office. </p>
+                        <p>This is a great place to host your holiday parties, business lunches, dinners, baby showers and much more! If you'd like to visit Us to discuss options For reservations, contact our office. </p>
 
-                    <p>Want to send comments or suggestion, please email us :
-                    FOR RESERVATIONS: please call:123-456-8899
-                    </p>
-                    <p>
+                        <p>Want to send comments or suggestion, please email us :
+                        FOR RESERVATIONS: please call:123-456-8899
+                        </p>
+                        <p>
 
-                    For any media inquiries, please contact:
-                    This is an example site,
-                    Taco@haven.com
-                    </p>
+                        For any media inquiries, please contact:
+                        This is an example site,
+                        Taco@haven.com
+                        </p>
                     </div>
                     
                     {/* </Container> */}
